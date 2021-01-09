0cea27c5141648f0a519f696f067f6ebdd8763b5
Anton Moskovchenko <moskovanton@gmail.com>
2021-01-09 21:37:50 +0300

    ExceptionHandler added

---
+++ src/main/java/com/notan/spring/rest/exception_handling/NoSuchEmployeeException.java
@@ -0,0 +1,8 @@
+package com.notan.spring.rest.exception_handling;
+
+public class NoSuchEmployeeException extends RuntimeException {
+
+    public NoSuchEmployeeException(String message) {
+        super(message);
+    }
+}
